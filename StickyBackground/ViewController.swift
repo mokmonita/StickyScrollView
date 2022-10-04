24bf6d5e29d87bf6ac89749fe2a8ca14dacb0743
Monita <nita.m@pipay.com>
2022-10-04 09:45:29 +0700

    Initial Commit

---
+++ StickyBackground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickyBackground
+//
+//  Created by Monita Mok on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
